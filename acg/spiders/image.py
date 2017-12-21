2cef4099c413a1bbf76d6b16b62b51311e745617
Zhangxuri <980278122@qq.com>
2017-12-21 15:03:17 +0800

    修复文件路径问题与页面爬取不完全问题
    二级页面下的fenye内容在原版中未爬去，本次更新加入该功能

--- acg/spiders/image.py
+++ acg/spiders/image.py
@@ -14,10 +14,12 @@ class acgimages(scrapy.Spider):
 	count = 0
 	page = 1
 
-	def parse2(self,response):
+
+
+	def parse3(self,response):
 
 		image_url = response.xpath("//article[@class='article-content']//img/@src").extract()
-
+		print(image_url)
 		print("一共找到图片%d" % len(image_url))
 
 		for item in image_url:
@@ -25,32 +27,45 @@ class acgimages(scrapy.Spider):
 			item = urllib.parse.quote(item,safe='/:?=.')
 			if 'jpg' in item:
 				self.count = self.count + 1
-				path = os.path.dirname(os.path.abspath(__file__))+ '\..\img\\' + str(self.count) + ".jpg"
+				path = os.path.dirname(os.path.abspath(__file__))+ '/../img/' + str(self.count) + ".jpg"
 				self.imageSave(item,path)
 			if 'png' in item:
 				self.count = self.count + 1
-				path = os.path.dirname(os.path.abspath(__file__))+ '\..\img\\' + str(self.count) + ".png"
+				path = os.path.dirname(os.path.abspath(__file__))+ '/../img/' + str(self.count) + ".png"
 				self.imageSave(item,path)
 
-		if self.page < 1180:
-			self.page = self.page + 1
+
+	def parse2(self,response):
+
+		pages_url = response.xpath("//div[@class='fenye']//a/@href").extract()
+		num=1
+		if len(pages_url)==0:
+			pages = response.xpath("//h1[@class='article-title']//a/@href").extract()
+			yield scrapy.Request(pages[0], callback=self.parse3)
+		else:
+			for pages in pages_url[:len(pages_url)-1]:
+				pages=pages[:len(pages)-1]+str(num)
+				num=num+1
+				yield scrapy.Request(pages,callback=self.parse3)
+
 
 	def parse(self,response):
 
-		pages_url = response.xpath("//div[@class='card-item']//a/@href").extract()
+		pages_url = response.xpath("//div[@class='card-item']//h3//a/@href").extract()
 		print("一共找到二级页面%d" % len(pages_url))
 		for pages in pages_url:
-			yield scrapy.Request(pages,callback = self.parse2)
-
+			yield scrapy.Request(pages, callback=self.parse2)
+		if self.page < 1180:
+			self.page = self.page + 1
 		next_url = "http://www.acg.fi/anime/page/%d" % self.page
 		yield scrapy.Request(next_url,callback = self.parse)
 
 	def imageSave(self,item,path):
 		maxsize = 512
 		req = urllib.request.Request(item)
-		req.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36")  
+		req.add_header("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36")
 		req.add_header("GET",item)
-		req.add_header("Host","http://www.acg.fi")  
+		req.add_header("Host","img.gov.com.de")
 		req.add_header("Referer",item)
 		res = urllib.request.urlopen(req).read()
 		image = np.asarray(bytearray(res),dtype="uint8")
@@ -68,5 +83,5 @@ class acgimages(scrapy.Spider):
 		cv2.imwrite(path,cutImage)
 		print('image is save in ' + path)
 
+	print("pageend,total:%d" % count)
 
-	
